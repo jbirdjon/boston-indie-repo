0a7ef41fae0e5de608e84ac60137422e2dfc0ab4
Jonathan Wallis <jonathanwallis@jonathans-mbp.lan>
2024-12-23 17:40:11 -0600

    added some kind of frontend... gotta mkae it pretty now

---
+++ types/event.ts
@@ -0,0 +1,11 @@
+export interface Event {
+    id: string;
+    bands: string[];
+    venue: string;
+    startTime: string;
+    price: number;
+    age: number;
+    date: Date;
+    link: string;
+  }
+  
